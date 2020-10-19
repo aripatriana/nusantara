f4a4b560f4fe8079447352fc151d95bc2d94930a
aripatriana <ari.patriana@sigma.co.id>
2020-10-19 08:39:31 +0700

    -- deprecate map type of COMPOSITE caused by concept dualism

--- src/main/java/com/nusantara/automate/annotation/MapType.java
+++ src/main/java/com/nusantara/automate/annotation/MapType.java
@@ -2,7 +2,21 @@ package com.nusantara.automate.annotation;
 
 public enum MapType {
 
+	/**
+	 * Inject data from data file and session
+	 */
 	RETENTION,
+	
+	
+	/**
+	 * Inject data from session 
+	 */
 	LOCAL,
+
+	/**
+	 * Inject data from data file and session, but duplicate concept with RETENTION 
+	 * so this type is deprecated
+	 */
+	@Deprecated
 	COMPOSITE;
 }
