59ab8657a48d5f200920cad632e0f5d7a7a56c72
aripatriana <ari.patriana@sigma.co.id>
2020-12-12 05:37:48 +0700

    --remove unused import

--- src/main/java/com/nusantara/automate/action/common/OpenSubMenuAction.java
+++ src/main/java/com/nusantara/automate/action/common/OpenSubMenuAction.java
@@ -1,10 +1,6 @@
 package com.nusantara.automate.action.common;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
